43ebd3a51c72d9609b5fb6e935db58bf0739af26
rlaskatjr007-design <rlaskatjr007@gmail.com>
2026-03-26 13:25:02 +0900

    Initial Commit

---
+++ Folder Flow/Folder_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Folder_FlowApp.swift
+//  Folder Flow
+//
+//  Created by REV_iMAC2 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Folder_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
